3724c1365034abc49e70832e2cf75d2e1db2a11a
Johan Valero <Johan.Pool.1994@gmail.com>
2023-03-22 20:50:15 -0500

    Database modified and bot integration

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import openai
 import tkinter as tk
 import sqlite3
+import json
 import pandas as pd
+import re
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 vDATABASE_PATH = "resources/sql_lite.db"
@@ -10,12 +12,13 @@ vDataBaseConnection = sqlite3.connect(vDATABASE_PATH)
 
 def action_1(iEvent):
     vPrompt = vTextInput.get("1.0", "end-1c")
-    vSqlGenerated = nlp2sql(vPrompt)
-    print("\n")
-    print("\tPrompt user: ", vPrompt)
-    print("\tSQL generated: ", vSqlGenerated)
-    print("\n")
     try:
+        print("")
+        print("Prompt user: ", vPrompt)
+        print("Processing...")
+        vSqlGenerated = nlp2sql(vPrompt)
+        print("\tSQL generated: ", vSqlGenerated)
+        print("\n")
         vDataFrame = pd.read_sql_query(vSqlGenerated, con = vDataBaseConnection)
         print(vDataFrame[0:10])
     except Exception as ex:
@@ -25,13 +28,25 @@ def nlp2sql(iPromptUser):
     vResult = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-                {"role": "system", "content": "Tu eres un bot que genera querys de SQLLite usando una entrada de texto en Español"},
-                {"role": "user", "content": """Tu tabla es TB_NETFLIX_DATA (show_id,type,title,director,cast,country,date_added,release_year,rating,duration,listed_in,description), dime todos los filmes que tegan un director nulo"""},
-                {"role": "assistant", "content": "SELECT * FROM TB_NETFLIX_DATA WHERE director IS NULL"},
-                {"role": "user", "content": iPromptUser}
+                {"role": "system", "content": "Tu eres un bot que genera querys de SQLLite en JSON usando una entrada de texto en Español"},
+                {"role": "user", "content": """Tu tabla es TB_NETFLIX_DATA (show_id,type,title,director,cast,country,date_added,release_year,rating,duration,listed_in,description), dime todos los filmes que tegan un director nulo. Con la consulta en un JSON."""},
+                {"role": "assistant", "content": "{\"sql\": \"SELECT * FROM TB_NETFLIX_DATA WHERE director IS NULL\"}"},
+                {"role": "user", "content": iPromptUser + ". Con la consulta en un JSON en el atributo sql."}
             ]
         )
-    return vResult["choices"][0]["message"]["content"]
+    vJSON = vResult["choices"][0]["message"]["content"]
+    try:
+        print("\tJSON generated: ", vJSON)
+        vSql = json.loads(vJSON)["sql"]
+    except Exception as ex:
+        print("\tError procesing JSON: ", ex)
+        vJSON = re.findall(
+                r'\b(?<!\\)([\'"])[^\1]*?\1(?!\1)',
+                vJSON
+            )[0]
+        print("\tNew JSON: ", vJSON)
+        vSql = json.loads(vJSON)["sql"]
+    return vSql
 
 vWindow = tk.Tk()
 vTextInput = tk.Text()
